4da660f45e440f0a73bee17ee324436a3d75f025
Alex Kharlamov <aakharlam@gmail.com>
2023-04-03 15:15:46 +0400

    feat: MediaCardRatig size

--- components/ContentLists/MediaCards/MediaCardRating.jsx
+++ components/ContentLists/MediaCards/MediaCardRating.jsx
@@ -2,7 +2,7 @@ import { Star } from "react-feather";
 
 function MediaCardRating({ value }) {
   return (
-    <div className="absolute bottom-[0px] left-[0px] z-20 m-2 flex items-center gap-1 rounded-lg bg-gray-500 bg-opacity-60 px-2 py-1.5">
+    <div className="absolute bottom-[0px] left-[0px] z-20 m-2 flex items-center gap-1 rounded-lg bg-gray-500 bg-opacity-70 px-1 py-0.5 md:px-2 md:py-1.5">
       <p className="text-base">{value}</p>
       <Star className="" />
     </div>
